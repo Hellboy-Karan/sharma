081617054a52320a42c4922d796dd470644a2a91
karan kumar sharma <sk5485633@gmail.com>
2021-05-04 09:58:31 +0530

    improve

--- src/Navbar.js
+++ src/Navbar.js
@@ -9,15 +9,15 @@ const Navbar = () => {
                 <navbar className='nav-class '>
                     <div className='navbar-brand'>
                         <div className='nav-brand'>
-                            <Link href='/' className='text'>Zoom</Link>
+                            <Link to='/' className='text'>Zoom</Link>
                         </div>
                     </div>
                     <div className='navbar-item'>
                         <ul className='nav-list'>
-                            <Link href='/' className='nav-link'><li><i class="fas fa-home"></i> Home</li></Link>
-                            <Link href='/about' className='nav-link'><li><i class="fas fa-info-circle"></i> About</li></Link>
-                            <Link href='/gallery' className='nav-link'><li><i class="far fa-image"></i> Gallery</li></Link>
-                            <Link href='#signup' className='nav-link'><li><i class="fas fa-user-plus"></i> Signup</li></Link>
+                            <Link to='/' className='nav-link'><li><i class="fas fa-home"></i> Home</li></Link>
+                            <Link to='/about' className='nav-link'><li><i class="fas fa-info-circle"></i> About</li></Link>
+                            <Link to='/gallery' className='nav-link'><li><i class="far fa-image"></i> Gallery</li></Link>
+                            <Link to='#signup' className='nav-link'><li><i class="fas fa-user-plus"></i> Signup</li></Link>
                         </ul>
                     </div>
                 </navbar>
